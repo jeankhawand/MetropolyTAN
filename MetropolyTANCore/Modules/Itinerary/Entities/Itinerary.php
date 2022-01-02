3013ee9b2d285fbcc32741d6c1f7bba3eead43ce
bokhQ <39876656+bokhQ@users.noreply.github.com>
2022-01-02 10:54:50 +0100

    Revert "Revert "Merge branch 'master' into feature/1-main-view""
    This reverts commit aa6f008091decc801c206940f994db9ec2de7518.

--- MetropolyTANCore/Modules/Itinerary/Entities/Itinerary.php
+++ MetropolyTANCore/Modules/Itinerary/Entities/Itinerary.php
@@ -4,15 +4,21 @@ namespace Modules\Itinerary\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\User\Entities\User;
+use Spatie\Permission\Traits\HasRoles;
 
 class Itinerary extends Model
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
 
     protected $fillable = [];
-    
+
     protected static function newFactory()
     {
         return \Modules\Itinerary\Database\factories\ItineraryFactory::new();
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- MetropolyTANCore/Modules/User/Database/Migrations/2014_10_12_000000_create_users_table.php
+++ MetropolyTANCore/Modules/User/Database/Migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use MStaack\LaravelPostgis\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -17,7 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->date('birth_date')->nullable();
+            $table->unsignedInteger('phone_number')->nullable();
             $table->string('address')->nullable();
+            $table->macAddress('device')->nullable();
+            $table->point('location')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
